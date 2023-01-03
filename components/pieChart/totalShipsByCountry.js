fa4b340ef3fd9690fe2a15a396e8a63de9e44b44
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-01-03 23:59:38 +0700

    Show total ships by country
    pie chart

---
+++ components/pieChart/totalShipsByCountry.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { useState, useEffect } from "react"
+import Image from 'next/image'
+
+import dynamic from 'next/dynamic';
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+//Font awesome classicon
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { } from "@fortawesome/free-solid-svg-icons"
+
+export default function TotalShipsByCountry() {
+    //Initial variable
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    var chart = [];
+
+    //Converter
+    const data = Object.values(items);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/ships/total/bycountry")
+        .then(res => res.json())
+            .then(
+            (result) => {
+                setIsLoaded(true);
+                setItems(result.data);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    },[])
+
+    function getSeries(val){
+        let catSeries = [];
+        val.forEach(e => { 
+            catSeries.push(parseInt(e.total));
+        });
+        return catSeries;
+    }
+
+    function getCategory(val){
+        let catData = [];
+        val.forEach(e => { 
+            catData.push(e.country);
+        });
+        return catData;
+    }
+
+    chart = {
+        series: getSeries(data),
+        options: {
+            labels: getCategory(data),
+            plotOptions: {
+                donut: {
+                  size: 200
+                }
+            }
+        }
+    };
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return (
+            <div>
+                {/* <Image
+                    src="/loading.gif"
+                    alt="Vercel Logo"
+                    className='loading-logo'
+                    width={100}
+                    height={100}
+                    priority
+                /> */}
+                <h5 className='text-center text-white mt-2 fst-italic'>Loading...</h5>
+            </div>
+        );
+    } else {
+        return (
+            <div className='custom-tbody' style={{padding:"6px"}}>
+                <h6 className='text-white'>Total Ships By Country</h6>
+                <div className="BalanceChart me-4">
+                    <Chart
+                        options={chart.options}
+                        series={chart.series}
+                        type="pie"
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+  
