c4fb0449ac68de756071712541fb7609312c55b9
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-01-03 23:04:18 +0700

    show all ships
    table

---
+++ components/table/allShips.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { useState, useEffect } from "react"
+import Image from 'next/image'
+
+const AllShips = () => {
+    //Initial variable
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    //Converter
+    const data = Object.values(items);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/ships/limit/15/order/ASC?page=1")
+        .then(res => res.json())
+            .then(
+            (result) => {
+                setIsLoaded(true);
+                setItems(result.data.data);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    },[])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return (
+            <div>
+                {/* <Image
+                    src="/loading.gif"
+                    alt="Vercel Logo"
+                    className='loading-logo'
+                    width={100}
+                    height={100}
+                    priority
+                /> */}
+                <h5 className='text-center text-white mt-2 fst-italic'>Loading...</h5>
+            </div>
+        );
+    } else {
+        return (
+            <div className='custom-tbody'>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Class</th>
+                            <th scope="col">Country</th>
+                            <th scope="col">Launch Year</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            items.map((val, i, index) => {
+                                return (
+                                    <tr key={i}>
+                                        <th scope="row">{val.name}</th>
+                                        <td>{val.class}</td>
+                                        <td>{val.country}</td>
+                                        <td>{val.launch_year}</td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+  
+export default AllShips;
+  
