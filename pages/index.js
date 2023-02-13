f39ec38e2276a66dbf15adb4bdcda4af9373f18b
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-02-13 23:33:11 +0900

    layout fixes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Navbar from '../components/navbar'
+import Navbar from '../../components/navbar/new_navbar'
 
 export default function Home() {
   return (
