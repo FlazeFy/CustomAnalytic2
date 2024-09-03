773cc8aa88955818a51521357a6dba05b59de825
Leonardho Rante S <79736302+FlazeFy@users.noreply.github.com>
2024-09-03 12:53:59 +0700

    [FE] Show discussion

--- pages/story/usecases/detail/getDiscussion.js
+++ pages/story/usecases/detail/getDiscussion.js
@@ -1,17 +1,49 @@
 import Axios from 'axios'
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Swal from 'sweetalert2';
 import AtomsText from '../../../../atoms/atoms_text'
-import GetDiscussionContainer from '../../../../components/containers/discussion_container'
 import { getStringValJson } from '../../../../modules/helpers/generator';
 import { getLocal } from '../../../../modules/storages/local';
+import MoleculesAlertBox from '../../../../molecules/molecules_alert_box';
 import MoleculesChatBox from '../../../../molecules/molecules_chat_box'
+import OrganismsDiscussionBox from '../../../../organisms/organisms_discussion_box';
 
 export default function GetDiscussion(props) {
+    const [error, setError] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [items, setItems] = useState(null)
     const userToken = getLocal('token_key')
     const [body, setBody] = useState(null)
     const [msgAll, setResMsgAll] = useState(null)
     const bodyRef = useRef(null)
+    const [currPage, setCurrPage] = useState(1)
+
+    useEffect(() => {
+        fetchData()
+    },[])
+
+    const fetchData = () => {
+        Swal.showLoading()
+        fetch(`http://127.0.0.1:8000/api/discussions/limit/14/order/desc/${props.id}?page=${currPage}`)
+        .then(res => res.json())
+            .then(
+            (result) => {
+                Swal.close()
+                setIsLoaded(true)
+                setItems(result.data.data)
+                setCurrPage(result.data.current_page)  
+            },
+            (error) => {
+                Swal.close()
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Something went wrong!",
+                })
+                setError(error)
+            }
+        )
+    } 
 
     // Services
     const handleSubmit = async (e) => {
@@ -35,6 +67,10 @@ export default function GetDiscussion(props) {
                         title: 'Success!',
                         text: response.data.message,
                         icon: 'success',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            fetchData()
+                        }
                     });
                 }
             } catch (error) {
@@ -76,19 +112,29 @@ export default function GetDiscussion(props) {
         }
     }
 
-    return (
-        <div>
-            <AtomsText body="Discussion" text_type="sub_heading"/>
-            {
-                props.data.map((val, i, index) => {
-                    return (
-                        <GetDiscussionContainer props={val}/>
-                    )
-                })
-            }
-            {
-                props.is_signed &&  <MoleculesChatBox handleSubmit={handleSubmit} messageRef={bodyRef} setMessage={(e)=>setBody(e.target.value)} is_with_attachment={true} context="discussion"/>
-            }
-        </div>
-    )
+    if (error) {
+        return <MoleculesAlertBox message={error.message} type='danger' context={`Story's feedback : ${router.query.slug}`}/>
+    } else if (!isLoaded) {
+        return (
+            <div>
+                <h5 className='text-center text-white mt-2 fst-italic'>Loading...</h5>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <AtomsText body="Discussion" text_type="sub_heading"/>
+                {
+                    items.map((dt, idx) => {
+                        return (
+                            <OrganismsDiscussionBox body={dt.body} role={dt.role} created_at={dt.created_at} created_by={dt.created_by}/>
+                        )
+                    })
+                }
+                {
+                    props.is_signed &&  <MoleculesChatBox handleSubmit={handleSubmit} messageRef={bodyRef} setMessage={(e)=>setBody(e.target.value)} is_with_attachment={true} context="discussion"/>
+                }
+            </div>
+        )
+    }
 }
